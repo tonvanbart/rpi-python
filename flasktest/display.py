12fffda2b98787c2899332d99510775c370ab885
ton <tonvanbart@gmail.com>
2025-12-12 01:50:06 +0100

    show message on GET

--- flasktest/display.py
+++ flasktest/display.py
@@ -4,10 +4,17 @@ import signal
 import sys
 
 app = Flask(__name__)
+sense = SenseHat()
+sense.set_rotation(180)
+count = 0
 
 @app.route('/')
 def hello_world():
+    # show something on the hat when the server is hit
+    global count
+    count = count + 1
+    sense.show_message('hit ' + str(count))
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0') # make server visible on local network
