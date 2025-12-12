dbbd9749be1f775bb0e93a9d008b5fb27f72240d
ton <tonvanbart@gmail.com>
2025-12-12 23:43:16 +0100

    added message display on Sense Hat

--- flasktest/display.py
+++ flasktest/display.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from sense_hat import SenseHat
 import signal
 import sys
@@ -14,7 +14,13 @@ def hello_world():
     global count
     count = count + 1
     sense.show_message('hit ' + str(count))
-    return 'Hello, World!'
+    return render_template('display.html', count=count)
+
+@app.route('/message', methods=['POST'])
+def show_message():
+    message = request.form['message']
+    sense.show_message(message)
+    return redirect(url_for('hello_world'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0') # make server visible on local network
+    app.run(host='0.0.0.0', debug=True) # make server visible on local network
